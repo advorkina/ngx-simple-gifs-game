f6ecd30a278adaa55d7086ad96f06b81d076427d
Anastasiya Dvorkina <anastasiya.dvorkina@s4m.com>
2018-06-27 10:34:26 +0200

    https for giphy

--- src/app/gifs-cards/gifs-cards.service.ts
+++ src/app/gifs-cards/gifs-cards.service.ts
@@ -4,12 +4,12 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class GifsCardsService {
-    private key = 'MaGTQKdkQcAnHj3yrH2FieHJGlhZ4gju';
+  private key = 'MaGTQKdkQcAnHj3yrH2FieHJGlhZ4gju';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getGifUrl(): Observable<any> {
-        const options = { params: new HttpParams().set('api_key', this.key) };
-        return this.http.get('http://api.giphy.com/v1/gifs/random', options);
-    }
+  getGifUrl(): Observable<any> {
+    const options = { params: new HttpParams().set('api_key', this.key) };
+    return this.http.get('https://api.giphy.com/v1/gifs/random', options);
+  }
 }
