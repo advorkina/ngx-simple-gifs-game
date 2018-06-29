a12a32a90a224b639a0991b43c07b668b0be6b17
Anastasia Dvorkina <anastasia.dvorkina@gmail.com>
2018-06-29 07:18:12 +0200

    making half of gifs

--- src/app/gifs-cards/gifs-cards.component.ts
+++ src/app/gifs-cards/gifs-cards.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { IGifCard } from './gif-card.interface';
-import { GifsCardsService } from './gifs-cards.service';
+import { Component, OnInit } from "@angular/core";
+import { IGifCard } from "./gif-card.interface";
+import { GifsCardsService } from "./gifs-cards.service";
 
 @Component({
-  selector: 'app-gifs-cards',
-  templateUrl: './gifs-cards.component.html',
-  styleUrls: ['../app.component.css', './gifs-cards.component.css']
+  selector: "app-gifs-cards",
+  templateUrl: "./gifs-cards.component.html",
+  styleUrls: ["../app.component.css", "./gifs-cards.component.css"]
 })
 export class GifsCardsComponent implements OnInit {
+  xNumber: number = 5;
+  yNumber: number = 4;
+  numberOfUniqueCards = (this.xNumber + this.yNumber) / 2;
+
   cards: IGifCard[][] = [];
-  constructor(private gifsCardsService: GifsCardsService) { }
+  constructor(private gifsCardsService: GifsCardsService) {}
 
   ngOnInit() {
     this.initialize();
@@ -25,8 +29,15 @@ export class GifsCardsComponent implements OnInit {
   }
 
   private initialize(): void {
-    for (let x = 0; x < 6; x++) {
-      for (let y = 0; y < 6; y++) {
+    let gifUrls = [];
+    for (let i = 0; i < this.numberOfUniqueCards; i++) {
+      this.gifsCardsService.getGifUrl().subscribe((r: any) => {
+        gifUrls[i] = r.data.embed_url;
+      });
+    }
+
+    for (let x = 0; x < this.xNumber; x++) {
+      for (let y = 0; y < this.yNumber; y++) {
         const card: IGifCard = {
           isOpen: false,
           url: undefined
